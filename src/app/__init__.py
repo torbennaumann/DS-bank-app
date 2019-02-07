1038dc04c5020a984bc8a64f442e82bdcebd97ec
Torben <t.naumann@zeppelin-university.net>
2019-02-07 12:23:39 +0100

    akutellerStand

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,5 @@
 from .bank import Bank
-
-__all__ = ['Bank']
-
 from .account import Account
-
-__all__ = ['Account']
-
 from .transaction import Transaction
 
-__all__ = ['Transaction']
+__all__ = ['Bank', 'Account', 'Transaction']
